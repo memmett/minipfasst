422c2645138411e038a955b6585be953e65afd23
Matthew Emmett <mwemmett@lbl.gov>
2014-12-01 13:50:20 -0800

    NS: Fix chop.

--- examples/navier-stokes/chop.f90
+++ examples/navier-stokes/chop.f90
@@ -3,9 +3,13 @@ program chop
   use feval
   use transfer
   use hooks
+  use probin
 
   type(pf_level) :: fine, crse
   real(pfdp), allocatable :: q_fine(:), q_crse(:)
+
+  npts = [ 128, 256, 256 ]
+  output = './'
 
   ! build a mock pfasst levels
   fine%level = 2
@@ -25,6 +29,6 @@ program chop
   ! interpolate and write fine_crse.dat
   q_fine = 0
   call interpolate(q_fine, q_crse, fine, crse, 0.d0)
-  call dump_velocity("fine_crse.dat", q_fine, fine)
+  call dump_velocity("fine_crse", q_fine, fine)
 
 end program chop
